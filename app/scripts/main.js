d180aae66017b7ee10247c7f32b09fe7252de970
Joe <vanhornRF@yahoo.com>
2017-05-09 13:33:16 -0500

    Work at Creed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
 	$('.nav-main .nav-item').click(function(event) {
 		$(this).toggleClass("active");
+		$('.dropdown-menu').toggleClass("show");
+	});
+	$('.sub-menu .dropdown-item').click(function(event){
+		$(this).toggleClass("active");
+		$('.sub-sub-menu').toggleClass("show");
+	});
+	$('.sub-sub-menu .dropdown-item').click(function(event){
+		$(this).toggleClass("active");
+		$(".call-to-action").toggleClass("show");
+	});
+	$(document).on('click', '.navbar .dropdown-menu', function (e) {
+	  e.stopPropagation();
 	});
 });
